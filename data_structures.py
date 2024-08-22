49dde62182c4c9531989b7f601ad0981b066dcb6
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-08-22 15:25:34 +0200

    Updated for 2024

--- data_structures.py
+++ data_structures.py
@@ -1,143 +1,108 @@
 # Imports
-import pytest
-import heapdict
+from __future__ import annotations  # Needed for typing Node class
+
 import warnings
+from typing import Any
+
 import binarytree
+import heapdict
 
-# Node class
+
+# Node class (do not change)
 class Node:
-    def __init__(self,data=None,next=None):
+    def __init__(self, data: Any = None, next: None | Node = None):
         self.data = data
-        self.next = next 
+        self.next = next
 
-# Add your implementations below and commit and push the file to your repository
-# (uit-dte-2602-f23/data-structures-<your github username>)
-# Every "pass" command should be replaced with your own code.
+
+# Add your implementations below, and commit and push the file to your repository
+
 
 class Stack:
     def __init__(self):
-        """ Initialize stack object, with head attribute """
+        """Initialize stack object, with head attribute"""
         pass
 
-    def push(self,data):
-        """ Add new node with data to stack """
+    def push(self, data: Any) -> None:
+        """Add new node with data to stack"""
         pass
 
-    def peek(self):
-        """ Return data from node on top of stack, without changing stack """
+    def peek(self) -> Node | None:
+        """Return data from node on top of stack, without changing stack"""
         pass
 
-    def pop(self):
-        """ Remove last added node and return its data """
+    def pop(self) -> Node:
+        """Remove last added node and return its data"""
         pass
 
 
 class Queue:
     def __init__(self):
-        """ Initialize queue object with head and tail """
+        """Initialize queue object with head and tail"""
         pass
 
-    def enqueue(self,data):
-        """ Add node with data to queue """
+    def enqueue(self, data: Any) -> None:
+        """Add node with data to queue"""
         pass
 
-    def peek(self):
-        """ Return data from head of queue without changing the queue """
+    def peek(self) -> Node | None:
+        """Return data from head of queue without changing the queue"""
         pass
-            
-    def dequeue(self):
-        """ Remove node from head of queue and return its data """
+
+    def dequeue(self) -> Node:
+        """Remove node from head of queue and return its data"""
         pass
-        
+
 
 class EmergencyRoomQueue:
     def __init__(self):
-        """ Initialize emergency room queue, use heapdict as property 'queue' """
+        """Initialize emergency room queue, use heapdict as property 'queue'"""
         pass
 
-    def add_patient_with_priority(self,patient_name,priority):
-        """ Add patient name and priority to queue 
-        
+    def add_patient_with_priority(self, patient_name: str, priority: int) -> None:
+        """Add patient name and priority to queue
+
         # Arguments:
         patient_name:   String with patient name
         priority:       Integer. Higher priority corresponds to lower-value number.
         """
         pass
 
-    def update_patient_priority(self,patient_name,new_priority):
-        """ Update the priority of a patient which is already in the queue 
-                
+    def update_patient_priority(self, patient_name: str, new_priority: int) -> None:
+        """Update the priority of a patient which is already in the queue
+
         # Arguments:
         patient_name:   String, name of patient in queue
         new_priority:   Integer, updated priority for patient
+
         """
         pass
 
-    def get_next_patient(self):
-        """ Remove highest-priority patient from queue and return patient name 
-        
+    def get_next_patient(self) -> str:
+        """Remove highest-priority patient from queue and return patient name
+
         # Returns:
         patient_name    String, name of patient with highest priority
         """
         pass
-    
 
-class IndexList:
-    def __init__(self):
-        """ Create IndexList object with head """
-        pass
-     
-    def insert(self, data, index):
-        """ Create new node with data and insert at given index 
-        
-        # Arguments:
-        data:       Content of node to be inserted
-        index:      Index of node to be inserted. The node is inserted into the list 
-                    before any existing nodes with the given index.
-                    Index must be an integer in the range [0,1,...,N]
-                    where N is the number of elements in the list.
-                    If the list is empty, insert(data,0) creates the first node.
-                    If index == N, the node is inserted after the last node in the list.
-                    If index > N, an IndexError is raised.
-        """
-        pass
-
-    def peek(self,index):
-        """ Return data of node at given index without changing list """
-        pass
-
-    def pop(self, index):
-        """ Remove and return data of node at given index """
-        pass
-
-    def __str__(self):
-        """ String representation of list (helper method for debugging)"""
-        # No need to change this method
-        list_string = ''
-        current = self.head
-        index = 0
-        while current is not None:
-            list_string += str(current.data) + f'({index}) -> '
-            current = current.next
-            index += 1
-        return list_string
-    
 
 class BinarySearchTree:
-    def __init__(self,root=None):
-        """ Initialize binary search tree
+    def __init__(self, root: binarytree.Node | None = None):
+        """Initialize binary search tree
 
         # Inputs:
         root:    (optional) An instance of binarytree.Node which is the root of the tree
 
         # Notes:
         If a root is supplied, validate that the tree meets the requirements
-        of a binary search tree (see property binarytree.Node.is_bst ). If not, raise ValueError.
+        of a binary search tree (see property binarytree.Node.is_bst ). If not, raise
+        ValueError.
         """
-        pass 
-    
-    def insert(self, value):
-        """ Insert a new node into the tree (binarytree.Node object)
+        pass
+
+    def insert(self, value: float | int) -> None:
+        """Insert a new node into the tree (binarytree.Node object)
 
         # Inputs:
         value:    Value of new node
@@ -149,8 +114,7 @@ class BinarySearchTree:
         """
         pass
 
-    def __str__(self):
-        """ Return string representation of tree (helper function for debugging) """
-        if self.root:
-            return(str(self.root))
-        
+    def __str__(self) -> str | None:
+        """Return string representation of tree (helper function for debugging)"""
+        if self.root is not None:
+            return str(self.root)
